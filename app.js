a42e7e9982660ea2b98874720162c43fcbe50bea
oferka <ofer.karp@gmail.com>
2016-09-20 05:50:50 +0300

    identify the bot

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.post('/hello', function (req, res, next) {
   var triggerText = req.body.text;
   var triggerWord = req.body.trigger_word;
   var botPayload = {
-    text : triggerWord + ', ' + userName + ', ' + triggerText
+    text : userName + ', ' + triggerText
   };
   // Loop otherwise..
   if (userName !== 'slackbot') {
