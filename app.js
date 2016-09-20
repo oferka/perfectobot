d100046c69b30a06a17f906b9fd53f1bc124a8dc
oferka <ofer.karp@gmail.com>
2016-09-20 05:31:24 +0300

    initial content

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 1337;
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// test route
+app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
+
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
+
+app.post('/hello', function (req, res, next) {
+  var userName = req.body.user_name;
+  var triggerText = req.body.text;
+  var triggerWord = req.body.trigger_word;
+  var botPayload = {
+    text : triggerWord + ', ' + userName + ', ' + triggerText
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
