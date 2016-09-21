b3e7b9941708c968407baf59267846b343bd8a5c
oferka <ofer.karp@gmail.com>
2016-09-21 11:58:11 +0300

    indent

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var app = express();
 var port = process.env.PORT || 1337;
 
 // body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // test route
-app.get('/', function (req, res) { res.status(200).send('Hello Perfecto Bot'); });
+app.get('/', function (req, res) {
+  res.status(200).send('Hello Perfecto Bot');
+});
 
 app.listen(port, function () {
   console.log('Perfecto bot is listening on port ' + port);
